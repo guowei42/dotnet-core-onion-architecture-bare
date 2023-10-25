bca2ac9efbc538f65e5410a69ef4edcfd946aadf
Guo Wei <65711401+guowei42@users.noreply.github.com>
2023-10-25 15:33:26 +0800

    add some skeleton

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using API;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+    }
+}
